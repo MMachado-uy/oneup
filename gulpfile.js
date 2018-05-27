8fc215e8fb3d84b04978a06b2cb30bbb147ee7e9
Mauricio Machado <macmauricio@gmail.com>
2018-05-26 21:43:26 -0300

    Refined gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('styles', function(cb) {
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(concat('main.css'))
-        .pipe(gulp.dest('dist/styles'))
+        //.pipe(gulp.dest('dist/styles'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .on('error', function (err) {
@@ -50,7 +50,7 @@ gulp.task('scripts', ['styles'], function(cb) {
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/scripts'))
+        //.pipe(gulp.dest('dist/scripts'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .on('error', function (err) {
